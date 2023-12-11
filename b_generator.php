ec8fd6de53fbf56c15839a940e3de49b7e60276e
oberon10 <prosperbernrad@gmail.com>
2023-12-11 07:07:20 +0100

    updated

--- b_generator.php
+++ b_generator.php
@@ -6,7 +6,23 @@
             <div class="container" style="padding-top: 2%;">
                 <center><h2>Welcome to Birthday Card Generator!</h2></center>
             
-           
+           <?php
+           if($_SERVER["REQUEST_METHOD"] == "POST"){
+            //`fname`, `lname`, `address`, `city`, `state`, `email`, `password`, `zip`)
+            $fname = $_POST["fname"];
+            $lname = $_POST["lname"];
+            
+            
+          if (mysqli_query($conn, $sql)) {
+            echo "New record created successfully";
+          } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+          }
+          
+          mysqli_close($conn);
+          
+          }
+           ?>
             <div id="card">
 
             </div>
